850fac97f6357084a349e8b10755d2dbd2d37384
SivanZihorev <115364144+SivanZihorev@users.noreply.github.com>
2024-08-14 17:30:17 +0300

    Initial commit

---
+++ app/src/main/java/com/example/codemasterapp/Quiz/QuizPage.kt
@@ -0,0 +1,62 @@
+package com.example.codemasterapp.Quiz
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.codemasterapp.R
+import com.example.codemasterapp.databinding.FragmentQuizPageBinding
+import com.google.firebase.database.FirebaseDatabase
+
+
+class QuizPage : Fragment() {
+
+    private lateinit var binding: FragmentQuizPageBinding
+    private lateinit var quizModelList: MutableList<QuizModel>
+    private lateinit var adapter: QuizListAdapter
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = FragmentQuizPageBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        quizModelList = mutableListOf()
+        getDataFromFirebase()
+    }
+
+    private fun setupRecyclerView() {
+        binding.progressBar.visibility = View.GONE
+        adapter = QuizListAdapter(quizModelList)
+        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerView.adapter = adapter
+    }
+
+    private fun getDataFromFirebase() {
+        binding.progressBar.visibility = View.VISIBLE
+        FirebaseDatabase.getInstance().reference
+            .get()
+            .addOnSuccessListener { dataSnapshot ->
+                if (dataSnapshot.exists()) {
+                    for (snapshot in dataSnapshot.children) {
+                        val quizModel = snapshot.getValue(QuizModel::class.java)
+                        if (quizModel != null) {
+                            quizModelList.add(quizModel)
+                        }
+                    }
+                }
+                setupRecyclerView()
+            }
+    }
+
+}
